e2f59098d19e479a4133da24748df254cbbfd4b9
Austin Shymanski <austin.shymanski@gmail.com>
2017-04-30 18:55:09 -0400

    the === and math

--- index.php
+++ index.php
@@ -9,8 +9,26 @@ $password = 'password';
 
 if ($password=='austin') {
   echo 'This is the correct password';
-} else {
+} else if ($password=='james'){
   echo 'This is the wrong password!';
+} else {
+  echo 'You are really bad at this.';
+}
+
+$num1 = 10;
+$num2 = 2;
+
+$result = ($num1 + $num2) / 2;
+
+echo $result;
+
+$number1 = 10.5;
+$number2 = '10.5';
+
+if ($number1===$number2) {
+  echo 'Equal.';
+} else {
+  echo 'Not equal.';
 }
 
 ?>
