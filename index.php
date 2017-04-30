e5eac2be5595729e2a8c55b6f5feee0710673382
Austin Shymanski <austin.shymanski@gmail.com>
2017-04-30 18:25:37 -0400

    working on basic control flow

--- index.php
+++ index.php
@@ -5,4 +5,12 @@ $age = 27;
 
 echo "My name is $name and I am $age years old.";
 
+$password = 'password';
+
+if ($password=='austin') {
+  echo 'This is the correct password';
+} else {
+  echo 'This is the wrong password!';
+}
+
 ?>
