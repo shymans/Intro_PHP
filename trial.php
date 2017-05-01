998ba3a2926bac4fb2737ab6abc6f2e9b65e2759
Austin Shymanski <ShymAust@cscprovidence.ca>
2017-05-01 11:59:42 -0400

    $_GET

--- trial.php
+++ trial.php
@@ -1,3 +1,20 @@
+<form action="trial.php" method="GET">
+	Name:<br><input type="text" name="name"><br>
+	Age:<br><input type="text" name="age" size="5"><br><br>
+	<input type="submit" value="Submit">
+</form>
+
 <?php
+
+$name = $_GET['name'];
+$age = $_GET['age'];
+
+if (isset($name)&&isset($age)) {
+	if (!empty($name)&&!empty($age)) {
+		echo 'I am '.$name.' and I am '.$age.' years old.';
+	} else {
+		echo 'Nothing entered';
+	}
+}
 
 ?>
