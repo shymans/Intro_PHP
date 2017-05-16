8d707a66a5dfa0e64ae4681c7a25d0c3953e2adb
Austin Shymanski <ShymAust@cscprovidence.ca>
2017-05-16 10:07:29 -0400

    Working with cookies

---
+++ cookies.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+$exp = time() + 86400;
+setcookie("name", "Austin", $exp);
+*/
+
+$exp_unset = time() - 86400;
+
+//echo $_COOKIE["name"];
+
+//print_r($_COOKIE);
+
+//unset cookie
+setcookie("name", "", $exp_unset);
+
+
+if (isset($_COOKIE['name']))
+	echo "Cookie is set";
+else
+	echo "Cookie is not set";
+?>
