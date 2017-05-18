686cbc711c769ef98170e1a77fd6a207555ba999
Austin Shymanski <ShymAust@cscprovidence.ca>
2017-05-18 11:54:15 -0400

    Splitting multiple white spaces

--- explode.php
+++ explode.php
@@ -6,7 +6,13 @@ $likes = explode(',', $likes);
 //print_r($likes);
 
 foreach($likes as $key => $like) {
-	echo '<a href="#">', $like, '</a><br>;
+	echo '<a href="#">', $like, '</a><br>';
 }
 
+
+//How to split a string with multiple white spaces
+$string = trim('Blue      car     sales');
+$keywords = preg_split('/[\s]+/', $string);
+
+print_r($keywords);
 ?>
