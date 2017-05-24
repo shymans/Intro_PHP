5ae17b12e824d7046e750927618415f91df520b3
Austin Shymanski <ShymAust@cscprovidence.ca>
2017-05-24 14:04:09 -0400

    Connecting to a DB

--- PDO/index.php
+++ PDO/index.php
@@ -2,4 +2,7 @@
 // Allows you to find the available drivers on your machine to use with PHP
 // var_dump(PDO::getAvailableDrivers());
 
+ini_set('display_errors', 'On');
+
+$db = new PDO('mysql:host=127.0.0.1;dpname=pdo', 'root', '');
 ?>
