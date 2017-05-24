aea3cf3ad56cd63a6be29a08f9096193271fda33
Austin Shymanski <ShymAust@cscprovidence.ca>
2017-05-24 14:21:33 -0400

    Query into DB

--- PDO/index.php
+++ PDO/index.php
@@ -6,13 +6,24 @@
 // ini_set('display_errors', 'On');
 
 
-$db = new PDO('mysql:host=127.0.0.1;dpname=pdo', 'root', '');
+$db = new PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
+
+/*
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// A simple way to catch an error
 
 try {
 	$db->query("INVALID");
 } catch (PDOException $e) {
 	die('Something went wrong.');
 }
+*/
+
+$db->query("
+	UPDATE users
+	SET last_name = 'Smith'
+	WHERE id = 1
+");
 
 ?>
