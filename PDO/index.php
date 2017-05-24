97f987868beb6b82c465826ed080b49e2f80e065
Austin Shymanski <ShymAust@cscprovidence.ca>
2017-05-24 14:11:30 -0400

    Catching exceptions

--- PDO/index.php
+++ PDO/index.php
@@ -2,7 +2,17 @@
 // Allows you to find the available drivers on your machine to use with PHP
 // var_dump(PDO::getAvailableDrivers());
 
-ini_set('display_errors', 'On');
+// For testing purposes only, never in a live environment
+// ini_set('display_errors', 'On');
+
 
 $db = new PDO('mysql:host=127.0.0.1;dpname=pdo', 'root', '');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+try {
+	$db->query("INVALID");
+} catch (PDOException $e) {
+	die('Something went wrong.');
+}
+
 ?>
