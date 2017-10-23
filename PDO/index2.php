50e48354f5789a5146d921920a82b1ffb578624b
Austin Shymanski <ShymAust@cscprovidence.ca>
2017-10-23 14:40:17 -0400

    Prepare method

--- PDO/index2.php
+++ PDO/index2.php
@@ -4,12 +4,26 @@ ini_set('display_errors', 'On');
 
 $db = new PDO('mysql:host=127.0.0.1;dbname=test', 'root', '');
 
+
+// Use quote to protect from an SQL injection -> Prepared methods are more useful and cleaner.
+/*
 if (!empty($_GET['user'])) {
 	$userId = $db->quote($_GET['user']);
 
 	$user = $db->query("SELECT * FROM users WHERE id = {$userId}");
 	var_dump($user->fetchObject());
-
+}
+*/
+// This is the prepare method that is cleaner than the quote method
+if (!empty($_GET['user'])) {
+	$user = $db->prepare("SELECT * FROM users WHERE id = :user_id");
+	
+	$user->execute([
+		'user_id' => $_GET['user'],
+	]);
+	
+	var_dump($user->fetchObject());
+}
 
 /* 
 $users = $db->query("SELECT count(id) AS count FROM users");
